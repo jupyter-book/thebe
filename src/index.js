60ef581452c855bbf3d3ccf9c316a7cc455beb6e
Min RK <benjaminrk@gmail.com>
2017-10-13 16:05:29 +0200

    fix window check

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import * as thebelab from "./thebelab.js";
 export * from "./thebelab.js";
 
-if (typeof window !== undefined) {
+if (typeof window !== "undefined") {
   window.thebelab = thebelab;
 }
