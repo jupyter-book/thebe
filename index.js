c02c3239f098ea5c0ef6250f1e4c109ce35b1884
Min RK <benjaminrk@gmail.com>
2017-10-19 17:02:54 +0200

    update example for 0.0.8

--- index.js
+++ index.js
@@ -22,5 +22,11 @@ function binderKernel() {
 }
 
 window.onload = function() {
+  thebelab.on("status", function(evt, data) {
+    console.log("Status changed:", data.status, data.message);
+    $(".thebe-status-button")
+      .attr("class", "thebe-status-button thebe-status-" + data.status)
+      .text(data.status);
+  });
   thebelab.bootstrap();
 };
