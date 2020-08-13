c11f29073f226904e3ad3a400c90d132849a3ed9
Min RK <benjaminrk@gmail.com>
2020-08-13 10:54:48 +0200

    [MNT] package name is thebe

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,7 @@ module.exports = {
   output: {
     filename: "index.js",
     path: path.resolve(__dirname, "lib"),
-    publicPath: "https://unpkg.com/thebelab@" + pkg.version + "/lib/",
+    publicPath: "https://unpkg.com/thebe@" + pkg.version + "/lib/",
   },
   plugins: [
     // Not using moment
