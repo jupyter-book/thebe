4b694a2fc134ada168f7d95539b81b719834f7ef
Steve Purves <steve.purves@earthanalytics.no>
2020-10-17 12:52:35 +0100

    dynamic import for bootstrap testing

--- test/bootstrap.spec.js
+++ test/bootstrap.spec.js
@@ -1,7 +1,18 @@
-import * as thebe from "../src";
+// import * as thebe from "../src";
 
 describe("bootstrap", () => {
-  it("should define events", () => {
+  // TODO: consider using karma-fixture
+  beforeEach(() => {
+    // stop automatic bootstrap
+    // `<script type="text/x-thebe-config">
+    // {
+    //   bootstrap: false
+    // }
+    // </script>
+    // `
+  })
+  it("should define events", async () => {
+    const thebe = await import("../src");
     expect(thebe).to.have.property("events");
   });
 })
