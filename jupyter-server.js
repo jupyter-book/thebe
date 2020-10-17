8e7951993241d297155d84844a6b371f0fadb29c
Steve Purves <steve.purves@earthanalytics.no>
2020-10-17 12:53:16 +0100

    pos. starting jupyter server via node

---
+++ jupyter-server.js
@@ -0,0 +1,8 @@
+import { JupyterServer } from '@jupyterlab/testutils';
+const server = new JupyterServer();
+
+server.start().then((msg) => {
+  console.log("Started Jupyter Server", msg)
+}).catch((msg) => {
+  console.log("Server start failed", msg)
+})
