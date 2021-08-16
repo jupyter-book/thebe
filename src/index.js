410e6d1a92eb5be0e212645658856d3adc80fd30
Steve Purves <stevejpurves@gmail.com>
2021-08-16 17:51:59 +0100

    exporting thebe's version of jquery partially addressing #215

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import * as thebelab from "./thebelab";
 export * from "./thebelab";
 export * from "./utils";
+import $ from "jquery";
 
 if (typeof window !== "undefined") {
   window.thebelab = thebelab;
+  window.thebelab.$ = $;
   const options = thebelab.mergeOptions();
   if (options["mountStatusWidget"]) {
     document.addEventListener("DOMContentLoaded", () => {
