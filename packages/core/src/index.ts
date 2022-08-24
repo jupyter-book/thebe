c7757f5d119654f4b509eea42ffc0fca4f10d557
stevejpurves <steve@curvenote.com>
2022-08-24 11:08:53 +0100

    🐥adjusting exports

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -7,7 +7,7 @@ import { connect, setupNotebook } from './thebe/api';
 
 export { default as ThebeServer } from './server';
 export { default as ThebeSession } from './session';
-export { default as ThebeNotebook } from './notebook';
+export { default as ThebeNotebook, CodeBlock } from './notebook';
 export { default as ThebeCell } from './cell';
 export { default as PassiveCellRenderer } from './passive';
 
