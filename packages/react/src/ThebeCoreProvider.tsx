ad845afc16b5a47122531e59e1a2cca64dc2e20d
Rowan Cockett <rowanc1@gmail.com>
2023-02-08 15:54:18 -0700

    📦 Export ThebeCore type

--- packages/react/src/ThebeCoreProvider.tsx
+++ packages/react/src/ThebeCoreProvider.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 
 // eslint-disable-next-line @typescript-eslint/consistent-type-imports
-type ThebeCore = typeof import('thebe-core');
+export type ThebeCore = typeof import('thebe-core');
 
 // Don;t know how to get rid of the undefined here in the case of an async provider
 const ThebeCoreContext = React.createContext<{ core?: ThebeCore; error?: string } | undefined>(
