fee875347c2b6007093dbfba19b98e3a0fe7fd81
stevejpurves <steve@curvenote.com>
2023-04-04 15:06:36 +0100

    🔼 building with `0.1.0-rc.0`, attempting to set lite options programatically

--- packages/lite/webpack.config.cjs
+++ packages/lite/webpack.config.cjs
@@ -1,8 +1,13 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const path = require('path');
 const webpack = require('webpack');
-const { DefinePlugin } = require('webpack');
+const { DefinePlugin, NormalModuleReplacementPlugin } = require('webpack');
 const HookShellScriptPlugin = require('hook-shell-script-webpack-plugin');
+
+const shimJS = path.resolve(__dirname, 'src', 'empty.js');
+function shim(regExp) {
+  return new NormalModuleReplacementPlugin(regExp, shimJS);
+}
 
 module.exports = {
   mode: 'production',
@@ -14,16 +19,13 @@ module.exports = {
     app: './src/index.ts',
   },
   plugins: [
+    shim(/\.(svg|ttf|eot|woff2|woff)/),
     new DefinePlugin({ 'process.env': {} }),
     new webpack.ProvidePlugin({
       currentScript: 'current-script-polyfill',
     }),
     new HookShellScriptPlugin({
-      afterEmit: [
-        'npm run build:post:shuffle',
-        'npm run build:post:service',
-        'node bin/stubContentsApi.js',
-      ],
+      afterEmit: ['node bin/stubContentsApi.js'],
     }),
   ],
   output: {
