744e2fb099d0329ed4c4d4e4b5e49fe3901bffd6
stevejpurves <steve@curvenote.com>
2023-04-27 13:22:14 +0100

    ✨improved react demo

--- apps/demo-react/src/App.tsx
+++ apps/demo-react/src/App.tsx
@@ -1,6 +1,6 @@
 import { useMemo } from 'react';
 import { ThebeCoreProvider, ThebeServerProvider } from 'thebe-react';
-import { Outlet } from 'react-router-dom';
+import { Link, NavLink, Outlet } from 'react-router-dom';
 import './App.css';
 import { Connect } from './Connect';
 
@@ -22,6 +22,21 @@ function App() {
     <div className="App">
       <div className="my-6">
         <h1 className="text-3xl font-bold">Notebooks & Widgets</h1>
+        <p>Available examples:</p>
+        <div className="flex justify-center space-x-2">
+          <NavLink
+            className={({ isActive }) => (isActive ? 'font-semibold underline' : 'hover:underline')}
+            to="/nb/widget-test"
+          >
+            widget basics
+          </NavLink>
+          <NavLink
+            className={({ isActive }) => (isActive ? 'font-semibold underline' : 'hover:underline')}
+            to="/nb/widget-test-ipympl"
+          >
+            ipympl
+          </NavLink>
+        </div>
       </div>
       <ThebeCoreProvider>
         <ThebeServerProvider connect={false} options={options}>
