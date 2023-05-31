fec0df5d787217e21736dafd309db81ee7d0fd9b
stevejpurves <steve@curvenote.com>
2023-05-31 15:36:37 +0100

    👊🏽 bumping paths in `thebe-react`

--- packages/react/src/ThebeServerProvider.tsx
+++ packages/react/src/ThebeServerProvider.tsx
@@ -72,9 +72,9 @@ export function ThebeServerProvider({
       server.connectToJupyterLiteServer({
         litePluginSettings: {
           '@jupyterlite/pyodide-kernel-extension:kernel': {
-            pipliteUrls: ['https://unpkg.com/@jupyterlite/pyodide-kernel@0.0.7/pypi/all.json'],
+            pipliteUrls: ['https://unpkg.com/@jupyterlite/pyodide-kernel@0.0.8/pypi/all.json'],
             pipliteWheelUrl:
-              'https://unpkg.com/@jupyterlite/pyodide-kernel@0.0.7/pypi/piplite-0.0.7-py3-none-any.whl',
+              'https://unpkg.com/@jupyterlite/pyodide-kernel@0.0.8/pypi/piplite-0.0.8-py3-none-any.whl',
           },
         },
       });
