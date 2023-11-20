c1b748b3872110d4940887694130d95e8b0aa1e5
stevejpurves <stevejpurves@users.noreply.github.com>
2023-11-20 12:52:32 +0000

    Deploy preview for PR 701 🛫

---
+++ pr-preview/pr-701/mathjax.js
@@ -0,0 +1,16 @@
+MathJax.Hub.Config({
+  extensions: ['tex2jax.js'],
+  jax: ['input/TeX', 'output/HTML-CSS'],
+  tex2jax: {
+    inlineMath: [
+      ['$', '$'],
+      ['\\(', '\\)'],
+    ],
+    displayMath: [
+      ['$$', '$$'],
+      ['\\[', '\\]'],
+    ],
+    processEscapes: true,
+  },
+  'HTML-CSS': { fonts: ['TeX'] },
+});
