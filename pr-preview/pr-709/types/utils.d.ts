0717489326723d4fc5bc0c9608de6d80c8f3a54b
stevejpurves <stevejpurves@users.noreply.github.com>
2023-11-21 20:37:35 +0000

    Deploy preview for PR 709 🛫

---
+++ pr-preview/pr-709/types/utils.d.ts
@@ -0,0 +1,4 @@
+import type { Options } from './options';
+export declare function stripPrompts({ selector, stripPrompts: prompts }: Options): void;
+export declare function stripOutputPrompts({ selector, stripOutputPrompts: prompts }: Options): void;
+export declare function randomId(): string;
